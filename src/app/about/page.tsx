84e8139b6e6e9c331e21d42129524a5deb9cca2d
yahyaraees <yahyaraees667@gmail.com>
2024-12-04 01:31:51 +0500

    first commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
     <div>
       <section className='about'>
         <div className='about-contact'>
-          <h2 >About Us</h2>
-          <p>Welcome to [Rolex],
-             the ultimate destination for Rolex enthusiasts and connoisseurs. 
-             Our passion for luxury timepieces is reflected in our curated collection
-              of the finest Rolex watches. We are dedicated to delivering unparalleled quality,
-               timeless elegance, and exceptional craftsmanship. Whether you're a seasoned collector or 
-               exploring the world of luxury watches for the first time, our mission is to guide you through 
-               every step with expertise and care. Discover the legacy of Rolex with us, where every moment is crafted to perfection.</p>
+          <h2>About Us</h2>
+          <p>
+            Welcome to [Rolex], the ultimate destination for Rolex enthusiasts and connoisseurs. 
+            Our passion for luxury timepieces is reflected in our curated collection of the finest Rolex watches. 
+            We are dedicated to delivering unparalleled quality, timeless elegance, and exceptional craftsmanship. 
+            Whether you&apos;re a seasoned collector or exploring the world of luxury watches for the first time, 
+            our mission is to guide you through every step with expertise and care. Discover the legacy of Rolex with us, 
+            where every moment is crafted to perfection.
+          </p>
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default About
-
+export default About;
